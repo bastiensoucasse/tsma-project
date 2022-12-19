e133ec01695d6d1938dc13f350da621c0726ddbd
Bastien Soucasse <bastien.soucasse@etu.u-bordeaux.fr>
2022-12-19 23:49:34 +0100

    Basic data augmentation (only for NNs)

--- neural_networks.py
+++ neural_networks.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator
 
 import datasets
 import models
@@ -12,36 +13,49 @@ import submissions
 EPOCHS = 20
 BATCH_SIZE = 32
 
+
+def usage(message: str | None = None, quit: bool = True):
+    '''
+    Prints a possible message and the program usage, and quits if needed.
+    '''
+
+    if message is not None:
+        print(message)
+
+    print(f'Usage: {sys.argv[0]} <dataset> <model> [\'data_augmentation\'].')
+    print(f'    <dataset>: {datasets.DATASETS}.')
+    print(f'    <model>: {models.MODELS}.')
+
+    if quit:
+        exit(-1)
+
+
 if __name__ == '__main__':
     # Check the usage.
-    if len(sys.argv) != 3:
-        print(f'Usage: {sys.argv[0]} <dataset> <model>.')
-        print(f'    <dataset>: {datasets.DATASETS}.')
-        print(f'    <model>: {models.MODELS}.')
-        exit(-1)
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        usage()
 
     # Retrieve the dataset name.
     dataset_name = sys.argv[1].casefold()
     if dataset_name not in datasets.DATASETS:
-        print(f'Dataset \'{dataset_name}\' unknown.')
-        print(f'Available datasets: {datasets.DATASETS}.')
-        exit(-1)
+        usage(message=f'Dataset \'{dataset_name}\' unknown.')
 
     # Retrieve the model name.
     model_name = sys.argv[2].casefold()
     if model_name not in models.MODELS:
-        print(f'Model \'{model_name}\' unknown.')
-        print(f'Available models: {models.MODELS}.')
-        exit(-1)
+        usage(message=f'Model \'{model_name}\' unknown.')
+
+    # Retrieve the data augmentation state.
+    if len(sys.argv) == 4 and sys.argv[3].casefold() != 'data_augmentation':
+        usage()
+    data_augmentation = True if len(sys.argv) == 4 and sys.argv[3].casefold() == 'data_augmentation' else False
 
     # Load the data.
     (ids_train, x_train, y_train), (ids_val, x_val, y_val), (ids_test, x_test) = datasets.load(dataset_name, hot_one_encode=True, split=True)
 
     # Define the input shape.
     if len(x_train.shape) <= 2:
-        print(f'Dataset \'{dataset_name}\' incompatible.')
-        print(f'The data must not be 1D.')
-        exit(-1)
+        usage(f'Dataset \'{dataset_name}\' incompatible: the data must not be 1D.')
     input_shape = (x_train.shape[1], x_train.shape[2], 1)
 
     # Summarize the data.
@@ -50,12 +64,30 @@ if __name__ == '__main__':
     print(f'Test data shape: {x_test.shape}.')
 
     # Define the model.
-    model = models.load(model_name, input_shape=input_shape)
+    model = models.load(model_name, input_shape)
     print(f'Model: {model_name}.')
+
+    if data_augmentation:
+        # Define the data augmentation.
+        datagen = ImageDataGenerator(
+            featurewise_center=True,
+            featurewise_std_normalization=True,
+            rotation_range=20,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            horizontal_flip=True
+        )
+        datagen.fit(x_train)
+
+        # Update dataset name.
+        dataset_name += '_augmented'
 
     # Train the model.
     start_time = time.time()
-    hist = model.fit(x=x_train, y=y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(x_val, y_val))
+    if data_augmentation:
+        hist = model.fit(datagen.flow(x_train, y_train, batch_size=BATCH_SIZE), steps_per_epoch=x_train.shape[0] // 32, epochs=EPOCHS, validation_data=(x_val, y_val))
+    else:
+        hist = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(x_val, y_val))
     training_time = time.time() - start_time
 
     # Evaluate the model.
